af78e1110c5277ba63d0c507620cb62aace583ee
Edwin Okoronkwo <edwinokoronkwo@gmail.com>
2025-07-30 09:45:18 -0600

    End of Section 2

---
+++ src/components/Providers.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React, {ReactNode} from 'react'
+import {HeroUIProvider} from "@heroui/system";
+
+export default function Providers({children}: {children: ReactNode}) {
+    return (
+        <HeroUIProvider>
+            {children}
+        </HeroUIProvider>
+    )
+}
