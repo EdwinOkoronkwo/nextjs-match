b13ba4e09602795d35b573b4df8a9f277ea18d3d
Edwin Okoronkwo <edwinokoronkwo@gmail.com>
2025-07-30 20:00:34 -0600

    End of Section 3

---
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import {zodResolver} from "@hookform/resolvers/zod";
+import {registerSchema, RegisterSchema} from '@/lib/schemas/registerSchema';
+import {Card, CardBody, CardHeader} from "@nextui-org/card";
+import {GiPadlock} from "react-icons/gi";
+import {Input} from "@nextui-org/react";
+import {Button} from "@heroui/button";
+import {registerUser} from "@/app/actions/authActions";
+
+export default function RegisterForm() {
+    const {register, handleSubmit, setError, formState: {errors, isValid, isSubmitting}} = useForm<RegisterSchema>({
+        resolver: zodResolver(registerSchema),
+        mode: 'onTouched'
+    })
+
+    const onSubmit = async(data: RegisterSchema) => {
+        const result = await registerUser(data);
+        if(result.status == 'success') {
+            console.log('User successfully registered');
+        }else {
+            if(Array.isArray(result.error)){
+                result.error.forEach((e) => {
+                    const fieldName = e.path.join('.')
+                    setError(fieldName, { message: e.message })
+                })
+            }else {
+                setError('root.serverError', {message: result.error})
+            }
+        }
+    }
+    return (
+        <Card className="w-2/5 mx-auto">
+            <CardHeader className="flex flex-col items-center justify-center">
+                <div className="flex flex-col gap-2 items-center text-secondary">
+                    <div className="flex flex-row items-center gap-3">
+                        <GiPadlock size={30} />
+                        <h1 className="text-3xl font-semibold">Register</h1>
+                    </div>
+                    <p className="text-neutral-500">Welcome to NextMatch</p>
+                </div>
+            </CardHeader>
+            <CardBody>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="space-y-4">
+                        <Input label="Name"
+                               variant="bordered"
+                               {...register('name')}
+                               isInvalid={!!errors.name}
+                               errorMessage={errors.name?.message}
+                        />
+                        <Input label="Email"
+                               variant="bordered"
+                               {...register('email')}
+                               isInvalid={!!errors.email}
+                               errorMessage={errors.email?.message}
+                        />
+                        <Input label="Password" variant="bordered"
+                               type="password"
+                               {...register('password')}
+                               isInvalid={!!errors.password}
+                               errorMessage={errors.password?.message}
+                        />
+                        {errors.root?.serverError && (
+                            <p className="text-danger text-sm">{errors.root.serverError.message}</p>
+                        )}
+                        <Button  isDisabled={!isValid}
+                                 fullWidth color="secondary" type="submit"
+                                 isLoading={isSubmitting}
+                        >
+                            Register
+                        </Button>
+                    </div>
+                </form>
+
+            </CardBody>
+        </Card>
+    )
+}
---
+++ src/components/navbar/UserMenu.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import React from 'react'
+import {Session} from "next-auth";
+import {Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger} from "@nextui-org/react";
+import Link from "next/link";
+import {signOutUser} from "@/app/actions/authActions";
+
+type UserMenuProps = {
+    user: Session['user']
+}
+
+export default function UserMenu({user}: UserMenuProps) {
+    return (
+        <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+                <Avatar isBordered as="button" className="transition-transform"
+                        color="secondary" name={user?.name || 'user avatar'}
+                        size="sm" src={user?.image || '/images/user.png'}
+                />
+
+            </DropdownTrigger>
+            <DropdownMenu variant="flat" aria-label="User actions menu">
+                <DropdownSection showDivider>
+                    <DropdownItem key='signInAs ' isReadOnly as="span"  className="h-14 flex flex-row" aria-label="username">
+                        Signed in as {user?.name}
+                    </DropdownItem>
+
+                </DropdownSection>
+                <DropdownItem key='editProfile' as={Link} href="/members/edit">
+                    Edit profile
+                </DropdownItem>
+                <DropdownItem
+                    key="logOut"
+                    color="danger"
+                    onPress={async () => {
+                        try {
+                            await signOutUser();
+                        } catch (err: any) {
+                            // Silently ignore expected NEXT_REDIRECT error
+                            if (err.digest !== "NEXT_REDIRECT") {
+                                console.error(err);
+                            }
+                        }
+                    }}
+                >
+                    Log out
+                </DropdownItem>
+
+            </DropdownMenu>
+
+        </Dropdown>
+    )
+}
